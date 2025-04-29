ec57860c334861dd5bca612d43f9dd768563b5f4
ElyasM10 <eliasmaldonado445@gmail.com>
2025-04-29 13:38:07 -0300

    Se arreglo el problema de las pestañas en la barra superior

--- changuitasAppFront/auxiliares/BarraPestanasPerfil.tsx
+++ changuitasAppFront/auxiliares/BarraPestanasPerfil.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import EstilosPerfilUsuario from '../Pantallas/Usuario/estilos/EstilosPerfilUsuario'; 
+import { NavigationProp, useNavigation, useRoute } from '@react-navigation/native';
+import EstilosPerfilUsuario from '../Pantallas/Usuario/estilos/EstilosPerfilUsuario';
 import { RootStackParamList } from '../navegacion/AppNavigator';
 
 const BarraPestanasPerfil = () => {
-   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const route = useRoute(); // Usamos useRoute para obtener la pantalla actual
+  const [pestanaActiva, setPestanaActiva] = useState<string>('Perfil'); 
+
+  // Actualiza  a la pestaña activa cuando cambie la ruta
+  useEffect(() => {
+    const rutaActual = route.name; 
+    setPestanaActiva(rutaActual);
+  }, [route.name]);
 
   return (
     <View style={EstilosPerfilUsuario.barraPestanas}>
-      <TouchableOpacity style={EstilosPerfilUsuario.pestanaActiva} onPress={() => navigation.navigate('Home')}>
-        <Text style={EstilosPerfilUsuario.textoPestanaActiva}>Perfil</Text>
+      <TouchableOpacity
+        style={pestanaActiva === 'PerfilUsuario' ? EstilosPerfilUsuario.pestanaActiva : EstilosPerfilUsuario.pestanaInactiva}
+        onPress={() => navigation.navigate('PerfilUsuario')}
+      >
+        <Text style={pestanaActiva === 'PerfilUsuario' ? EstilosPerfilUsuario.textoPestanaActiva : EstilosPerfilUsuario.textoPestanaInactiva}>
+          Perfil
+        </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={EstilosPerfilUsuario.pestanaInactiva} onPress={() => navigation.navigate('EditarPerfil')}>
-        <Text style={EstilosPerfilUsuario.textoPestanaInactiva}>Editar</Text>
+      <TouchableOpacity
+        style={pestanaActiva === 'EditarPerfil' ? EstilosPerfilUsuario.pestanaActiva : EstilosPerfilUsuario.pestanaInactiva}
+        onPress={() => navigation.navigate('EditarPerfil')}
+      >
+        <Text style={pestanaActiva === 'EditarPerfil' ? EstilosPerfilUsuario.textoPestanaActiva : EstilosPerfilUsuario.textoPestanaInactiva}>
+          Editar
+        </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={EstilosPerfilUsuario.pestanaInactiva} onPress={() => navigation.navigate('MisServicios')}>
-        <Text style={EstilosPerfilUsuario.textoPestanaInactiva}>Mis servicios</Text>
+      <TouchableOpacity
+        style={pestanaActiva === 'MisServicios' ? EstilosPerfilUsuario.pestanaActiva : EstilosPerfilUsuario.pestanaInactiva}
+        onPress={() => navigation.navigate('MisServicios')}
+      >
+        <Text style={pestanaActiva === 'MisServicios' ? EstilosPerfilUsuario.textoPestanaActiva : EstilosPerfilUsuario.textoPestanaInactiva}>
+          Mis servicios
+        </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={EstilosPerfilUsuario.pestanaInactiva} onPress={() => navigation.navigate('UsuariosBloqueados')}>
-        <Text style={EstilosPerfilUsuario.textoPestanaInactiva}>Bloqueados</Text>
+      <TouchableOpacity
+        style={pestanaActiva === 'UsuariosBloqueados' ? EstilosPerfilUsuario.pestanaActiva : EstilosPerfilUsuario.pestanaInactiva}
+        onPress={() => navigation.navigate('UsuariosBloqueados')}
+      >
+        <Text style={pestanaActiva === 'UsuariosBloqueados' ? EstilosPerfilUsuario.textoPestanaActiva : EstilosPerfilUsuario.textoPestanaInactiva}>
+          Bloqueados
+        </Text>
       </TouchableOpacity>
     </View>
   );
