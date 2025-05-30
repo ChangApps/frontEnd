9b49ecc8228a2a99d4c9c3235a155fa1394f4be1
ElyasM10 <eliasmaldonado445@gmail.com>
2025-05-30 13:36:15 -0300

    Se cambio API_URL

--- changuitasAppFront/auxiliares/API_URL.js
+++ changuitasAppFront/auxiliares/API_URL.js
@@ -1,4 +1,4 @@
-//const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'http://127.0.0.1:8000';
 
 //Para android studio la ip  y  python manage.py runserver 0.0.0.0:8000
 //const API_URL ='http://10.0.2.2:8000/';
@@ -6,5 +6,5 @@
 
 //Para expo go en el telefono la ip de cada uno (Si no la saben abrir una terminal escribir ipconfig y la que dice direccion ipv4)  
 // y  python manage.py runserver 0.0.0.0:8000
-const API_URL ='http://192.168.1.12:8000/'
+//const API_URL ='http://<IP.LOCAL>:8000/'
 export default API_URL;
