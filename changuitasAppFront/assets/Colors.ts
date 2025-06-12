8ca4f93c6f253fe2f5a2c129b9399afa0907ecbc
ElyasM10 <eliasmaldonado445@gmail.com>
2025-06-12 09:23:57 -0300

    Se agrego archivo colors.ts(en asset) y snackbar en componente

---
+++ changuitasAppFront/assets/Colors.ts
@@ -0,0 +1,12 @@
+const Colors = {
+  fondo: '#191A2E',
+  naranja: '#FC6A30',
+  degradeTop: '#191A2E',
+  degradeBottom: '#505494',
+  blancoTexto: '#F2F2F2',
+  grisTexto: '#70706f',
+  grisBoxes: '#333237',
+  grisFondo: '#292929',
+};
+
+export default Colors;
---
+++ changuitasAppFront/componentes/CustomSnackbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Snackbar } from 'react-native-paper';
+
+type Props = {
+  visible: boolean;
+  setVisible: (value: boolean) => void;
+  message: string;
+};
+
+const CustomSnackbar = ({ visible, setVisible, message }: Props) => {
+  return (
+    <Snackbar
+      visible={visible}
+      onDismiss={() => setVisible(false)}
+      duration={Snackbar.DURATION_SHORT}
+      style={{
+        position: 'absolute',
+        bottom: 20,  
+        left: 0,
+        right: 0,
+        zIndex: 100000,  // Alto para asegurarse de que esté encima de otros elementos
+      }}
+    >
+      {message}
+    </Snackbar>
+  );
+};
+
+export default CustomSnackbar;
